bda6e6c30e5daf131c5c1c9763b3d03fc5b574ab
jx3536 <jx3536@163.com>
2018-03-07 16:50:20 +0800

    [+]添加配置中心客户端

---
+++ config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package org.redrock.saltfish.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("word")
+	String word;
+
+	@GetMapping("/word")
+	public String word() {
+		return word;
+	}
+}
