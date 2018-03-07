1de290c05052fb7a82c1bc68072528663f0f5674
jx3536 <jx3536@163.com>
2018-03-07 20:02:37 +0800

    [+]添加rabbitmq中间件

--- config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
@@ -3,9 +3,11 @@ package org.redrock.saltfish.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
