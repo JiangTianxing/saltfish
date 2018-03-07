6bab08f9e2fb392819b662717c48c25f61a2a148
jx3536 <jx3536@163.com>
2018-03-07 17:59:44 +0800

    更改配置文件

--- config-server/src/main/java/org/redrock/saltfish/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/org/redrock/saltfish/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package org.redrock.saltfish.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
