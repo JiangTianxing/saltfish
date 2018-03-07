7b3eaf975c665a1fb0e88769c8d87e8b670f0ffd
jx3536 <jx3536@163.com>
2018-03-07 21:24:54 +0800

    更改配置文件

--- config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
@@ -1,14 +1,8 @@
 package org.redrock.saltfish.configclient;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
-@RestController
 @SpringBootApplication
 public class ConfigClientApplication {
 
@@ -16,11 +10,4 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
-	@Value("${word}")
-	String word;
-
-	@GetMapping("/word")
-	public String word() {
-		return word;
-	}
 }
---
+++ config-client/src/main/java/org/redrock/saltfish/configclient/WordController.java
@@ -0,0 +1,19 @@
+package org.redrock.saltfish.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class WordController {
+
+    @Value("${word}")
+    String word;
+
+    @GetMapping("/word")
+    public String word() {
+        return word;
+    }
+}
