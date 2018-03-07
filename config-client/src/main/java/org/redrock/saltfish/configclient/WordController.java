75d64da283a9e91479791b73c940c57496befa4b
jx3536 <jx3536@163.com>
2018-03-07 23:21:18 +0800

    更改配置文件

--- config-client/src/main/java/org/redrock/saltfish/configclient/WordController.java
+++ config-client/src/main/java/org/redrock/saltfish/configclient/WordController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WordController {
 
-    @Value("${word}")
+    @Value("${word:hhhh}")
     String word;
 
     @GetMapping("/word")
