d674a441eaddfc537a662d590dea614b15e9c252
jx3536 <jx3536@163.com>
2018-03-09 23:57:03 +0800

    更改配置文件

--- config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/org/redrock/saltfish/configclient/ConfigClientApplication.java
@@ -2,6 +2,7 @@ package org.redrock.saltfish.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
