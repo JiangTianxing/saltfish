340ca99e283e67cb678d37f4ae3b90ed018e20ef
jx3536 <jx3536@163.com>
2018-03-14 22:57:05 +0800

    开始创建系统构建

--- discovery/src/main/java/org/redrock/saltfish/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/org/redrock/saltfish/discovery/DiscoveryApplication.java
@@ -3,6 +3,9 @@ package org.redrock.saltfish.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,5 +13,16 @@ public class DiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+	/**
+	 * 解除csrf
+	 */
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+		}
 	}
 }
