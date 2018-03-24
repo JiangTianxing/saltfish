9eef9edc5a34d53538307b8917dbc1269953cbe0
jx3536 <jx3536@163.com>
2018-03-25 00:46:40 +0800

    添加zipkin追踪

---
+++ zipkin/src/main/java/org/redrock/saltfish/zipkin/ZipkinApplication.java
@@ -0,0 +1,14 @@
+package org.redrock.saltfish.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
